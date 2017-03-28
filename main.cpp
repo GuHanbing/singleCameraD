b9762f9812ede9551478a32f7393c18a54f99ea4
PI <xuxinting1995523@qq.com>
2017-03-28 14:43:47 +0800

    change control codes

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 #include <unistd.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
-
-uchar *buffer;
+#include <vector>
 
 #define IMAGEWIDTH 640
 #define IMAGEHEIGHT 480
@@ -19,7 +19,7 @@ uchar *buffer;
 #define TRUE 1
 #define FALSE 0
 
-#define FILE_VIDEO1 "/dev/video1"
+#define FILE_VIDEO1 "/dev/video0"
 
 static int fd;
 struct v4l2_streamparm setfps;
@@ -31,64 +31,66 @@ struct v4l2_buffer buf;
 enum   v4l2_buf_type type;
 
 int init_v4l2(void);
-int v4l2_grab(void);
-
-
 
 int main()
 {
-        printf("first~~\n");
-        if(init_v4l2() == FALSE){
-            printf("Init fail~~\n");
-            exit(1);
-        }
-        printf("second~~\n");
-        if(v4l2_grab() == FALSE){
-            printf("grab fail~~\n");
-            exit(2);
-        }
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        printf("third~~\n");
-        cvNamedWindow("one",CV_WINDOW_AUTOSIZE);
-        IplImage* img;
-        CvMat cvmat;
-        int i = 100;
-        double t;
-        while(1){
-                t = (double)cvGetTickCount();
-                ioctl(fd,VIDIOC_DQBUF,&buf);
-                buf.index = 0;
-                cvmat = cvMat(IMAGEHEIGHT,IMAGEWIDTH,CV_8UC3,(void*)buffer);//CV_8UC3
-                //t = (double)cvGetTickCount();
-                img = cvDecodeImage(&cvmat,1);
-                //t=(double)cvGetTickCount()-t;
-                //printf("used time is %gms\n",(t/(cvGetTickFrequency()*1000)));
-                if(!img)    printf("No img\n");
-                cvShowImage("one",img);
-                cvReleaseImage(&img);
-                ioctl(fd,VIDIOC_QBUF,&buf);
-                if((cvWaitKey(1)&255) == 27)    exit(0);
-                t=(double)cvGetTickCount()-t;
-                printf("used time is %gms\n",(t/(cvGetTickFrequency()*1000)));
-        }
 
-        ioctl(fd,VIDIOC_STREAMOFF,&type);
-        return 0;
+    printf("first~~\n");
+    if(init_v4l2() == FALSE){
+        printf("Init fail~~\n");
+        exit(1);
+    }
+
+
+    CvCapture *capture;
+    capture = cvCaptureFromCAM(0);
+
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_HEIGHT,720) << std::endl;
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_FRAME_WIDTH,1280) << std::endl;
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_BRIGHTNESS,124) << std::endl;
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_HUE,90) << std::endl;
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_SATURATION,128) << std::endl;
+    std::cout << cvSetCaptureProperty(capture,CV_CAP_PROP_FPS,10) << std::endl;
+
+    if(capture == NULL)
+        std::cout << "Open Camera Error" << std::endl;
+    IplImage *get;
+    get = cvQueryFrame(capture);
+
+    if(get == NULL)
+        std::cout << "capture Error" << std::endl;
+
+    std::cout << "color : " << get->channelSeq << std::endl;
+
+    cv::Mat change;
+    change = cv::Mat(get);
+//    cv::cvtColor(change,change,cv::COLOR_BGR2RGB);
+
+    std::cout << "channels : " << change.channels() << std::endl;
+    cv::cvtColor(change,change,CV_BGR2RGB);
+//    std::cout << "color : " << change << std::endl;
+
+    cvNamedWindow("0");
+    cvShowImage("0",get);
+
+    cv::imshow("1",change);
+    cvWaitKey(0);
+
+    cvReleaseImage(&get);
+    cvReleaseCapture(&capture);
+    return 0;
 }
 
 int init_v4l2(void){
-        if ((fd = open(FILE_VIDEO1, O_RDWR)) == -1){
-            printf("Opening video device error\n");
-            return FALSE;
-        }
-        if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1){
-                printf("unable Querying Capabilities\n");
-                return FALSE;
-        }
-        else
-/*
-        {
+    if ((fd = open(FILE_VIDEO1, O_RDWR)) == -1){
+        printf("Opening video device error\n");
+        return FALSE;
+    }
+    if (ioctl(fd, VIDIOC_QUERYCAP, &cap) == -1){
+        printf("unable Querying Capabilities\n");
+        return FALSE;
+    }
+    else{
         printf( "Driver Caps:\n"
                 "  Driver: \"%s\"\n"
                 "  Card: \"%s\"\n"
@@ -101,109 +103,47 @@ int init_v4l2(void){
                 cap.version,
                 cap.capabilities);
 
-        }
-
-        if((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == V4L2_CAP_VIDEO_CAPTURE){
-            printf("Camera device %s: support capture\n",FILE_VIDEO1);
-        }
-        if((cap.capabilities & V4L2_CAP_STREAMING) == V4L2_CAP_STREAMING){
-            printf("Camera device %s: support streaming.\n",FILE_VIDEO1);
-        }
-*/
-        fmtdesc.index = 0;
-        fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        printf("Support format: \n");
-        while(ioctl(fd,VIDIOC_ENUM_FMT,&fmtdesc) != -1){
-            printf("\t%d. %s\n",fmtdesc.index+1,fmtdesc.description);
-            fmtdesc.index++;
-        }
-        //set fmt
-        fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        fmt.fmt.pix.width = IMAGEWIDTH;
-        fmt.fmt.pix.height = IMAGEHEIGHT;
-        fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG; //*************************V4L2_PIX_FMT_YUYV****************
-        fmt.fmt.pix.field = V4L2_FIELD_NONE;
-
-        if (ioctl(fd, VIDIOC_S_FMT, &fmt) == -1){
-            printf("Setting Pixel Format error\n");
-            return FALSE;
-        }
-        if(ioctl(fd,VIDIOC_G_FMT,&fmt) == -1){
-            printf("Unable to get format\n");
-            return FALSE;
-        }
-//        else
-
-/*        {
-            printf("fmt.type:\t%d\n",fmt.type);
-            printf("pix.pixelformat:\t%c%c%c%c\n",fmt.fmt.pix.pixelformat & 0xFF,(fmt.fmt.pix.pixelformat >> 8) & 0xFF,\
-                   (fmt.fmt.pix.pixelformat >> 16) & 0xFF, (fmt.fmt.pix.pixelformat >> 24) & 0xFF);
-            printf("pix.height:\t%d\n",fmt.fmt.pix.height);
-            printf("pix.field:\t%d\n",fmt.fmt.pix.field);
-        }
-*/
-/*
-        setfps.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        setfps.parm.capture.timeperframe.numerator = 100;
-        setfps.parm.capture.timeperframe.denominator = 100;
-        printf("init %s is OK\n",FILE_VIDEO1);
-*/
-        return TRUE;
-}
-
-int v4l2_grab(void){
-    //struct v4l2_requestbuffers req = {0};
-    //4  request for 4 buffers
-    req.count = 1;
-    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    req.memory = V4L2_MEMORY_MMAP;
-
-    if (ioctl(fd, VIDIOC_REQBUFS, &req) == -1)
-    {
-        printf("Requesting Buffer error\n");
-        return FALSE;
     }
-    //5 mmap for buffers
-    buffer = (uchar*)malloc(req.count * sizeof(*buffer));
-    if(!buffer){
-        printf("Out of memory\n");
-        return FALSE;
-    }
-    unsigned int n_buffers;
-    for(n_buffers = 0;n_buffers < req.count; n_buffers++){
-    //struct v4l2_buffer buf = {0};
-    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    buf.memory = V4L2_MEMORY_MMAP;
-    buf.index = n_buffers;
-    if(ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1){
-        printf("Querying Buffer error\n");
-        return FALSE;
-        }
 
-    buffer = (uchar*)mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+    if((cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) == V4L2_CAP_VIDEO_CAPTURE){
+        printf("Camera device %s: support capture\n",FILE_VIDEO1);
+    }
+    if((cap.capabilities & V4L2_CAP_STREAMING) == V4L2_CAP_STREAMING){
+        printf("Camera device %s: support streaming.\n",FILE_VIDEO1);
+    }
 
-    if(buffer == MAP_FAILED){
-        printf("buffer map error\n");
-        return FALSE;
-        }
-    printf("Length: %d\nAddress: %p\n", buf.length, buffer);
-    printf("Image Length: %d\n", buf.bytesused);
+    fmtdesc.index = 0;
+    fmtdesc.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    printf("Support format: \n");
+    while(ioctl(fd,VIDIOC_ENUM_FMT,&fmtdesc) != -1){
+        printf("\t%d. %s\n",fmtdesc.index+1,fmtdesc.description);
+        fmtdesc.index++;
     }
-    //6 queue
-    for(n_buffers = 0;n_buffers <req.count;n_buffers++){
-        buf.index = n_buffers;
-        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-        buf.memory = V4L2_MEMORY_MMAP;
-        if(ioctl(fd,VIDIOC_QBUF,&buf)){
-            printf("query buffer error\n");
-            return FALSE;
-        }
-    }
-    //7 starting
-    type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    if(ioctl(fd,VIDIOC_STREAMON,&type) == -1){
-        printf("stream on error\n");
-        return FALSE;
-    }
+
+    struct v4l2_control control_exp;
+    control_exp.id = V4L2_CID_EXPOSURE_AUTO;
+    control_exp.value = V4L2_EXPOSURE_MANUAL;
+    ioctl(fd,VIDIOC_S_CTRL,&control_exp);
+
+    control_exp.id = V4L2_CID_EXPOSURE_ABSOLUTE;
+    control_exp.value = 100;
+    ioctl(fd,VIDIOC_S_CTRL,&control_exp);
+
+//    //set fmt
+//    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+//    fmt.fmt.pix.width = IMAGEWIDTH;
+//    fmt.fmt.pix.height = IMAGEHEIGHT;
+//    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG; //*************************V4L2_PIX_FMT_YUYV****************
+//    fmt.fmt.pix.field = V4L2_FIELD_ANY;
+
+//    if (ioctl(fd, VIDIOC_S_FMT, &fmt) == -1){
+//        printf("Setting Pixel Format error\n");
+//        return FALSE;
+//    }
+//    if(ioctl(fd,VIDIOC_G_FMT,&fmt) == -1){
+//        printf("Unable to get format\n");
+//        return FALSE;
+//    }
+    close(fd);
     return TRUE;
 }
